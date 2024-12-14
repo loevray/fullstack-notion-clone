b712eea3055f93e874806054226dcd570bfa1fb4
loevray <rei980213@gmail.com>
2024-12-14 13:58:56 +0900

    Style: fold structure

---
+++ src/models/db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017"; // MongoDB URI
+const client = new MongoClient(uri);
+const dbName = "notion_clone";
+
+let db;
+
+const connectDB = async () => {
+  if (!db) {
+    await client.connect();
+    db = client.db(dbName);
+  }
+  return db;
+};
+
+module.exports = connectDB;
