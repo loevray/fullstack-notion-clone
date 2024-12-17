3a4924cc2997f8732232817b2cc218d02321f5f6
loevray <rei980213@gmail.com>
2024-12-18 08:45:53 +0900

    Rename: buildTree

--- api-server/documents/documentsRepository.js
+++ api-server/documents/documentsRepository.js
@@ -1,4 +1,7 @@
 const { connectDB, client, getClient } = require("../models/db");
+const {
+  buildTreeFromMaterializedPath,
+} = require("./utils/buildTreeFromMaterializedPath");
 const { getToday } = require("./utils/getToday");
 
 const COLLECTION_NAME = "documents";
@@ -20,41 +23,11 @@ async function getNextSequenceValue(collectionName) {
   return result.sequence_value;
 }
 
-const buildTree = (documents) => {
-  const idToNodeMap = {}; // 문서를 ID 기반으로 빠르게 조회하기 위한 맵
-  const rootNodes = []; // 최상위 문서를 저장
-
-  // 1. 맵에 저장하면서 path를 제거한 기본 구조 초기화
-  for (const doc of documents) {
-    const { _id, content, updatedAt, createdAt, ...rest } = doc; // path 제거
-    idToNodeMap[doc._id] = { ...rest, documents: [] };
-  }
-
-  // 2. 부모-자식 관계 설정
-  for (const doc of documents) {
-    const { _id, path } = doc;
-
-    if (!path) {
-      // path가 null이면 루트 문서
-      rootNodes.push(idToNodeMap[_id]);
-    } else {
-      // 부모 ID 추출
-      const parentIds = path.split(",").filter(Boolean);
-      const parentId = parentIds[parentIds.length - 1];
-      if (idToNodeMap[parentId]) {
-        idToNodeMap[parentId].documents.push(idToNodeMap[_id]);
-      }
-    }
-  }
-
-  return rootNodes;
-};
-
 async function getDocuments() {
   const db = await connectDB();
 
   const documents = await db.collection(COLLECTION_NAME).find().toArray();
-  const documentsTree = buildTree(documents);
+  const documentsTree = buildTreeFromMaterializedPath(documents);
 
   return documentsTree;
 }
