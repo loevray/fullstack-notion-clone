ec188498897238fecb5c485470a9a2b320bb10cf
loevray <rei980213@gmail.com>
2024-12-18 14:16:39 +0900

    Fix: CORS error fix

--- api-server/documents/documentsController.js
+++ api-server/documents/documentsController.js
@@ -7,16 +7,34 @@ const {
 async function getDocumentController(req, res) {
   const { documentId } = req.body;
   const document = await getDocumentById(documentId);
-  return res.status(200).json(document);
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
+  return res.end(JSON.stringify(document));
 }
 async function getDocumentListController(req, res) {
   const documentList = await getDocuments();
-  return res.status(200).json(documentList);
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
+  return res.end(JSON.stringify(documentList));
 }
 async function createDocumentController(req, res) {
   const { parentId, document } = req.body;
   const newDocument = await createDocument({ parentId, document });
-  return res.status(201).json(newDocument);
+  res.writeHead(201, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
+  return res.end(JSON.stringify(newDocument));
 }
 async function updateDocumentController(req, res) {
   return;
