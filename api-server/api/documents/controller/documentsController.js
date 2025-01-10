3bcd345c886c4ab6e19e6473f190c692598e1c55
loevray <rei980213@gmail.com>
2025-01-10 19:34:09 +0900

    Refactor: Layered Architecture using DI

--- api-server/api/documents/controller/documentsController.js
+++ api-server/api/documents/controller/documentsController.js
@@ -1,58 +1,61 @@
-const {
-  createDocument,
-  getDocumentById,
-  getDocuments,
-  updateDocument,
-  deleteDocument,
-} = require("../service/documentsService");
+function documentsController(documentsService) {
+  const {
+    createDocument,
+    getDocumentById,
+    getDocuments,
+    updateDocument,
+    deleteDocument,
+  } = documentsService();
 
-async function getDocumentController(req, res) {
-  const documentId = req.params.id;
-  const document = await getDocumentById(documentId);
+  async function getDocumentController(req, res) {
+    const documentId = req.params.id;
+    const document = await getDocumentById(documentId);
 
-  res.writeHead(200);
-  return res.end(JSON.stringify(document));
+    res.writeHead(200);
+    return res.end(JSON.stringify(document));
+  }
+
+  async function getDocumentListController(req, res) {
+    const documentList = await getDocuments();
+    res.writeHead(200);
+    return res.end(JSON.stringify(documentList));
+  }
+
+  async function createDocumentController(req, res) {
+    const { parent, title } = req.body;
+
+    const createdDocument = await createDocument({ title, parentId: +parent });
+    res.writeHead(201);
+    return res.end(JSON.stringify(createdDocument));
+  }
+
+  async function updateDocumentController(req, res) {
+    const { title, content } = req.body;
+    const documentId = req.params.id;
+
+    const updatedDocument = await updateDocument({
+      documentId,
+      newDocument: { title, content },
+    });
+
+    res.writeHead(200);
+    return res.end(JSON.stringify(updatedDocument));
+  }
+
+  async function deleteDocumentController(req, res) {
+    const documentId = req.params.id;
+    await deleteDocument(documentId);
+
+    res.writeHead(204);
+    return res.end();
+  }
+  return {
+    getDocumentController,
+    getDocumentListController,
+    createDocumentController,
+    updateDocumentController,
+    deleteDocumentController,
+  };
 }
 
-async function getDocumentListController(req, res) {
-  const documentList = await getDocuments();
-  res.writeHead(200);
-  return res.end(JSON.stringify(documentList));
-}
-
-async function createDocumentController(req, res) {
-  const { parent, title } = req.body;
-
-  const createdDocument = await createDocument({ title, parentId: +parent });
-  res.writeHead(201);
-  return res.end(JSON.stringify(createdDocument));
-}
-
-async function updateDocumentController(req, res) {
-  const { title, content } = req.body;
-  const documentId = req.params.id;
-
-  const updatedDocument = await updateDocument({
-    documentId,
-    newDocument: { title, content },
-  });
-
-  res.writeHead(200);
-  return res.end(JSON.stringify(updatedDocument));
-}
-
-async function deleteDocumentController(req, res) {
-  const documentId = req.params.id;
-  await deleteDocument(documentId);
-
-  res.writeHead(204);
-  return res.end();
-}
-
-module.exports = {
-  getDocumentController,
-  getDocumentListController,
-  createDocumentController,
-  updateDocumentController,
-  deleteDocumentController,
-};
+module.exports = documentsController;
--- api-server/api/documents/service/documentsService.js
+++ api-server/api/documents/service/documentsService.js
@@ -13,181 +13,187 @@ const { DOCUMENTS_COLLECTION, COUNTERS_COLLECTION, SEQUENCE_VALUE } =
 
 const { DEFAULT_CONTENT, DEFAULT_TITLE } = DocumentConstants;
 
-async function getNextSequenceValue(collectionName) {
-  const db = await connectDB();
+function documentsService() {
+  async function getNextSequenceValue(collectionName) {
+    const db = await connectDB();
 
-  try {
-    const result = await db.collection(COUNTERS_COLLECTION).findOneAndUpdate(
-      { _id: collectionName },
-      { $inc: { [SEQUENCE_VALUE]: 1 } },
-      {
-        returnDocument: "after",
-        returnOriginal: false,
-        upsert: true,
-        projection: { [SEQUENCE_VALUE]: 1 },
-      }
-    );
+    try {
+      const result = await db.collection(COUNTERS_COLLECTION).findOneAndUpdate(
+        { _id: collectionName },
+        { $inc: { [SEQUENCE_VALUE]: 1 } },
+        {
+          returnDocument: "after",
+          returnOriginal: false,
+          upsert: true,
+          projection: { [SEQUENCE_VALUE]: 1 },
+        }
+      );
 
-    return result[SEQUENCE_VALUE];
-  } catch (e) {
-    throw new DatabaseError(`Failed to get next sequence value: ${e.message}`);
-  }
-}
-
-async function getDocuments() {
-  const db = await connectDB();
-
-  try {
-    const documents = await db
-      .collection(DOCUMENTS_COLLECTION)
-      .find()
-      .toArray();
-
-    return buildTreeFromMaterializedPath(documents);
-  } catch (e) {
-    throw new DatabaseError(`Failed to get documents: ${e.message}`);
-  }
-}
-
-async function getDocumentById(id) {
-  const db = await connectDB();
-
-  try {
-    const document = await db.collection(DOCUMENTS_COLLECTION).findOne(
-      { _id: +id },
-      {
-        projection: { _id: 0 },
-      }
-    );
-
-    if (!document)
-      throw new NotFoundError(`${id}번째 문서를 찾을 수 없습니다!`);
-
-    return document;
-  } catch (e) {
-    throw new DatabaseError(`Failed to get document: ${e.message}`);
-  }
-}
-
-async function createDocument({ title = DEFAULT_TITLE, parentId = null }) {
-  const db = await connectDB();
-  const nextId = await getNextSequenceValue(DOCUMENTS_COLLECTION);
-
-  let parentDoc;
-  if (parentId) parentDoc = await getDocumentById(parentId);
-
-  const materializedPath = generateDocumentPath({
-    parentId,
-    parentPath: parentDoc?.materializedPath,
-  });
-
-  const today = getToday();
-
-  const newDocument = {
-    _id: nextId,
-    id: nextId,
-    title,
-    content: DEFAULT_CONTENT,
-    materializedPath,
-    createdAt: today,
-    updatedAt: today,
-  };
-
-  let result;
-
-  try {
-    result = await db.collection(DOCUMENTS_COLLECTION).insertOne(newDocument);
-  } catch (e) {
-    throw new DatabaseError(`Failed to create document: ${e.message}`);
+      return result[SEQUENCE_VALUE];
+    } catch (e) {
+      throw new DatabaseError(
+        `Failed to get next sequence value: ${e.message}`
+      );
+    }
   }
 
-  return await getDocumentById(result.insertedId);
-}
+  async function getDocuments() {
+    const db = await connectDB();
 
-async function updateDocument({ documentId, newDocument }) {
-  const db = await connectDB();
+    try {
+      const documents = await db
+        .collection(DOCUMENTS_COLLECTION)
+        .find()
+        .toArray();
 
-  if (newDocument.title?.length >= 20) {
-    throw new ValidationError("제목은 20자 이하여야 합니다.");
+      return buildTreeFromMaterializedPath(documents);
+    } catch (e) {
+      throw new DatabaseError(`Failed to get documents: ${e.message}`);
+    }
   }
 
-  try {
-    const result = await db.collection(DOCUMENTS_COLLECTION).findOneAndUpdate(
-      { _id: +documentId },
-      {
-        $set: {
-          ...newDocument,
-          updatedAt: getToday(),
+  async function getDocumentById(id) {
+    const db = await connectDB();
+
+    try {
+      const document = await db.collection(DOCUMENTS_COLLECTION).findOne(
+        { _id: +id },
+        {
+          projection: { _id: 0 },
+        }
+      );
+
+      if (!document)
+        throw new NotFoundError(`${id}번째 문서를 찾을 수 없습니다!`);
+
+      return document;
+    } catch (e) {
+      throw new DatabaseError(`Failed to get document: ${e.message}`);
+    }
+  }
+
+  async function createDocument({ title = DEFAULT_TITLE, parentId = null }) {
+    const db = await connectDB();
+    const nextId = await getNextSequenceValue(DOCUMENTS_COLLECTION);
+
+    let parentDoc;
+    if (parentId) parentDoc = await getDocumentById(parentId);
+
+    const materializedPath = generateDocumentPath({
+      parentId,
+      parentPath: parentDoc?.materializedPath,
+    });
+
+    const today = getToday();
+
+    const newDocument = {
+      _id: nextId,
+      id: nextId,
+      title,
+      content: DEFAULT_CONTENT,
+      materializedPath,
+      createdAt: today,
+      updatedAt: today,
+    };
+
+    let result;
+
+    try {
+      result = await db.collection(DOCUMENTS_COLLECTION).insertOne(newDocument);
+    } catch (e) {
+      throw new DatabaseError(`Failed to create document: ${e.message}`);
+    }
+
+    return await getDocumentById(result.insertedId);
+  }
+
+  async function updateDocument({ documentId, newDocument }) {
+    const db = await connectDB();
+
+    if (newDocument.title?.length >= 20) {
+      throw new ValidationError("제목은 20자 이하여야 합니다.");
+    }
+
+    try {
+      const result = await db.collection(DOCUMENTS_COLLECTION).findOneAndUpdate(
+        { _id: +documentId },
+        {
+          $set: {
+            ...newDocument,
+            updatedAt: getToday(),
+          },
         },
-      },
-      { returnNewDocument: true }
-    );
-    return result;
-  } catch (e) {
-    throw new DatabaseError(`Failed to update document: ${e.message}`);
+        { returnNewDocument: true }
+      );
+      return result;
+    } catch (e) {
+      throw new DatabaseError(`Failed to update document: ${e.message}`);
+    }
   }
+
+  async function getChildDocuments(parentId) {
+    const db = await connectDB();
+
+    try {
+      return await db
+        .collection(DOCUMENTS_COLLECTION)
+        .find({
+          materializedPath: { $regex: `,${parentId},` },
+        })
+        .toArray();
+    } catch (e) {
+      throw new DatabaseError(`Failed to get child documents: ${e.message}`);
+    }
+  }
+
+  async function updateDescendantsPath(parentDoc, childDoc) {
+    const newPath = generateDocumentPath({
+      parentId: parentDoc.id,
+      childPath: childDoc.materializedPath,
+      parentPath: parentDoc.materializedPath,
+    });
+
+    // 문서 업데이트
+    await updateDocument({
+      documentId: childDoc._id,
+      newDocument: { materializedPath: newPath },
+    });
+  }
+
+  async function deleteDocument(id) {
+    const db = await connectDB();
+    const documentToDelete = await getDocumentById(id);
+
+    if (!documentToDelete) {
+      throw new NotFoundError(`${id}번째 문서를 찾을 수 없습니다!`);
+    }
+
+    const childDocuments = await getChildDocuments(id);
+
+    //트랜잭션 이용해서 자식 경로 안전하게 처리하는 방법이 필요
+    if (childDocuments.length > 0) {
+      await Promise.all(
+        childDocuments.map((childDoc) =>
+          updateDescendantsPath(documentToDelete, childDoc)
+        )
+      );
+    }
+
+    // 문서 삭제
+    try {
+      return await db.collection(DOCUMENTS_COLLECTION).deleteOne({ _id: +id });
+    } catch (e) {
+      throw new DatabaseError(`Failed to delete document: ${e.message}`);
+    }
+  }
+
+  return {
+    getDocuments,
+    getDocumentById,
+    createDocument,
+    updateDocument,
+    deleteDocument,
+  };
 }
 
-async function getChildDocuments(parentId) {
-  const db = await connectDB();
-
-  try {
-    return await db
-      .collection(DOCUMENTS_COLLECTION)
-      .find({
-        materializedPath: { $regex: `,${parentId},` },
-      })
-      .toArray();
-  } catch (e) {
-    throw new DatabaseError(`Failed to get child documents: ${e.message}`);
-  }
-}
-
-async function updateDescendantsPath(parentDoc, childDoc) {
-  const newPath = generateDocumentPath({
-    parentId: parentDoc.id,
-    childPath: childDoc.materializedPath,
-    parentPath: parentDoc.materializedPath,
-  });
-
-  // 문서 업데이트
-  await updateDocument({
-    documentId: childDoc._id,
-    newDocument: { materializedPath: newPath },
-  });
-}
-
-async function deleteDocument(id) {
-  const db = await connectDB();
-  const documentToDelete = await getDocumentById(id);
-
-  if (!documentToDelete) {
-    throw new NotFoundError(`${id}번째 문서를 찾을 수 없습니다!`);
-  }
-
-  const childDocuments = await getChildDocuments(id);
-
-  //트랜잭션 이용해서 자식 경로 안전하게 처리하는 방법이 필요
-  if (childDocuments.length > 0) {
-    await Promise.all(
-      childDocuments.map((childDoc) =>
-        updateDescendantsPath(documentToDelete, childDoc)
-      )
-    );
-  }
-
-  // 문서 삭제
-  try {
-    return await db.collection(DOCUMENTS_COLLECTION).deleteOne({ _id: +id });
-  } catch (e) {
-    throw new DatabaseError(`Failed to delete document: ${e.message}`);
-  }
-}
-
-module.exports = {
-  getDocuments,
-  getDocumentById,
-  createDocument,
-  updateDocument,
-  deleteDocument,
-};
+module.exports = documentsService;
