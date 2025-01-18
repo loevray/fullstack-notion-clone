157ba0294df578dd4e9f620c9ba0a3d34ce7b606
loevray <rei980213@gmail.com>
2025-01-18 21:19:23 +0900

    Fix: constants typo fix

--- api-server/constants/database.js
+++ api-server/constants/database.js
@@ -2,7 +2,7 @@ const DatabaseConstants = Object.freeze({
   DATABASE_NAME: "notion_clone",
   DOCUMENTS_COLLECTION: "documents",
   COUNTERS_COLLECTION: "counters",
-  SEQUENCE_VALUE: 1,
+  SEQUENCE_VALUE: "sequence_value",
 });
 
 module.exports = {
