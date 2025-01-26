94f3758b24c3024de333613b5d7ea2667bdcc163
loevray <rei980213@gmail.com>
2025-01-26 15:18:11 +0900

    Fix: state depends on reducer name

--- web-server/public/js/main.js
+++ web-server/public/js/main.js
@@ -1,9 +1,14 @@
 import App from "./App.js";
 import documentsReducer from "./modules/documentsDuck.js";
+import { combineReducers } from "./utils/myRedux/combineReducers.js";
 import { createStore } from "./utils/myRedux/createStore.js";
 import thunk from "./utils/myRedux/thunk.js";
 
-const store = createStore(documentsReducer, thunk);
+const rootReducer = combineReducers({
+  documentsReducer,
+});
+
+const store = createStore(rootReducer, thunk);
 export { store };
 
 const $root = document.getElementById("root");
--- web-server/public/js/utils/myRedux/combineReducers.js
+++ web-server/public/js/utils/myRedux/combineReducers.js
@@ -14,5 +14,6 @@ export const combineReducers = (reducers) => {
     });
     return nextState;
   };
+
   return combinatedReducer;
 };
